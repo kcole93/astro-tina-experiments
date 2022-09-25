7b6ebbc89e5189a9d26a8a335944f9f2c526bb63
Kevin Cole <kcole93@gmail.com>
2022-09-25 18:57:40 -0400

    tinacms

--- .tina/schema.js
+++ .tina/schema.js
@@ -4,8 +4,8 @@ import { client } from './__generated__/client'
 
 const schema = defineSchema({
   config: {
-    token: '<Your Read Only Token>', // generated on app.tina.io,
-    clientId: '<Your Client ID>', // generated on app.tina.io
+    token: 'a4a7fb185a566f1a2d2d9ee560e358778a341a9c', // generated on app.tina.io,
+    clientId: 'a1d0abca-75e7-4c27-83fd-601f43de1a49', // generated on app.tina.io
     branch: 'main'
   },
   collections: [
--- src/comp/TinaAstro.jsx
+++ src/comp/TinaAstro.jsx
@@ -4,7 +4,7 @@ import { TinaEditProvider } from 'tinacms/dist/edit-state'
 
 export default (props) => {
   const branch = "main";
-  const clientId = "";
+  const clientId = "a1d0abca-75e7-4c27-83fd-601f43de1a49";
   const apiURL =
     process.env.NODE_ENV == "development"
       ? "http://localhost:4001/graphql"
