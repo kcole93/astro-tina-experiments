df02f17dd05e290bfa56cff1ad150122f3529b67
Kevin Cole <kcole93@gmail.com>
2022-09-25 16:49:42 -0400

    Init setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,23 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
-const chooseLayout = () => (tree, {
-  data
-}) => {
-  data.astro.frontmatter.layout = 'comp/Page.astro';
-};
-
+const chooseLayout =
+  () =>
+  (tree, { data }) => {
+    data.astro.frontmatter.layout = "comp/Page.astro";
+  };
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   markdown: {
-    remarkPlugins: [chooseLayout]
-  }
+    remarkPlugins: [chooseLayout],
+  },
+  vite: {
+    resolve: {
+      alias: {
+        path: "path-browserify",
+      },
+    },
+  },
 });
--- src/comp/TinaAstro.jsx
+++ src/comp/TinaAstro.jsx
@@ -1,11 +1,26 @@
-import { TinaAdmin } from 'tinacms'
+import { TinaAdmin } from "tinacms";
 import TinaProvider from '.tina/components/TinaProvider'
 import { TinaEditProvider } from 'tinacms/dist/edit-state'
 
 export default (props) => {
+  const branch = "main";
+  const clientId = "";
+  const apiURL =
+    process.env.NODE_ENV == "development"
+      ? "http://localhost:4001/graphql"
+      : `https://content.tinajs.io/content/${clientId}/github/${branch}`;
   return (
-    <TinaEditProvider>
-      <TinaAdmin />
+     
+       <TinaEditProvider
+      editMode={
+        <TinaProvider apiURL={apiURL}>
+          <TinaAdmin />
+        </TinaProvider>
+      }
+    >
+      <div>Need to be in edit mode!</div>
     </TinaEditProvider>
+    
+    
   )
 }
